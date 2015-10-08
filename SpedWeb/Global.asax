c3d88b942867fa34703282db213f67afb53fa338
daviddbarth@hotmail.com <daviddbarth@hotmail.com>
2015-10-07 19:44:18 -0500

    Initial Commit

---
+++ SpedWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpedWeb.MvcApplication" Language="C#" %>
---
+++ SpedWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpedWeb.Startup))]
+namespace SpedWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
